31c57e623c0aa8bc52ea3074116309b746495a7c
yashV0201 <vardhan.yash0201@gmail.com>
2024-02-03 04:32:09 +0530

    longest sum optimal added

--- longestSubArr.java
+++ longestSubArr.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class longestSubArr {
 
     static int subArr(int[] a, int k){
@@ -22,9 +25,37 @@ public class longestSubArr {
         return c_max;
     }
 
+    static int longestSub(int[] arr, int k ){ // approach: using hashing and reverse mathematics
+        Map<Integer, Integer> hm = new HashMap<>();
+        int maxLen=0;
+        int sum = 0;
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            if(sum == k) maxLen = Math.max(maxLen, i+1);
+
+            int rem = sum - k;
+
+            if(hm.containsKey(rem)){
+                int len = i- hm.get(rem);
+                maxLen = Math.max(maxLen, len);
+
+            }
+            
+            if(! hm.containsKey(rem)){
+                hm.put(sum, i);
+            }
+        }
+
+        return maxLen;
+
+
+    }
+
     public static void main(String[] args) {
         int a = subArr(new int[]{2,3,5,1,1,1,1,-1,2,2,2,9}, 9);
-        System.out.println(a);
+        int b = longestSub(new int[]{2,3,5,1,1,1,1,2,2,2,-1,9}, 4);
+        System.out.println(a +" "+b);
     }
     
 }
