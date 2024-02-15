6ab02445dbf53da98ce115879303d4a0ac70bf65
yashV0201 <vardhan.yash0201@gmail.com>
2024-02-15 16:38:24 +0530

    pascal triangle optimal

--- pascalsTriangle.java
+++ pascalsTriangle.java
@@ -9,10 +9,43 @@ public class pascalsTriangle {
 
         return ans;
     }
+
+    static int element(int n, int r){
+        return nCr(n-1, r-1);
+    }
+
+    static void row(int n){      //optimal solution  TC: O(N)
+        int ans = 1;
+        System.out.print(ans+" ");
+        for(int i=1; i<n; i++){
+            ans = ans*(n-i);
+            ans = ans/i;
+            System.out.print(ans+" ");
+
+        }
+
+    }
+
+    static void triangle(int n){      // optimal soln   TC: O(N*N);
+        for(int i=0; i<=n; i++){
+            row(i);
+            System.out.println();
+        }
+    }
+
+
+
     public static void main(String[] args) {
 
         int ncr = nCr(8,6);
-        System.out.println(ncr);
+        System.out.println();
+
+        int ele = element(5, 3);
+        System.out.println(ele);
+
+        row(6);
+
+        triangle(6);
         
     }
     
