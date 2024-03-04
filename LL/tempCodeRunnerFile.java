77a05bc1532b9938731fb69b2d30125f3351f9bd
yashV0201 <vardhan.yash0201@gmail.com>
2024-03-05 02:01:50 +0530

    search for loop

--- LL/tempCodeRunnerFile.java
+++ LL/tempCodeRunnerFile.java
@@ -1,6 +1,16 @@
-if(carry != 0){
-        //     Node newst = new Node(carry);
-        //     temp3.next =newst;
-        //     temp3 = temp3.next;
+static boolean optimal(Node head){
+    //     Map<Node,Integer> hm = new HashMap<>();
 
-        // }
+    //     Node temp = head;
+    //     while(temp != null){
+    //         if(hm.containsKey(temp)) return true;
+    //         else{
+    //             hm.put(temp,hm.getOrDefault(temp, 0)+1);
+    //         }
+
+    //         temp = temp.next;
+    //     }
+
+    //     return false;
+
+    // }
