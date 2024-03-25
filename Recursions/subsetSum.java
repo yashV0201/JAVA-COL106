430eddb407c25fc744f1a6f6f3a6c456008af645
yashV0201 <vardhan.yash0201@gmail.com>
2024-03-26 03:37:46 +0530

    subsets of an array(non repeating ans) amazing approach

--- Recursions/subsetSum.java
+++ Recursions/subsetSum.java
@@ -19,10 +19,25 @@ public class subsetSum {
 
     }
 
+    static void subset(int[] arr, int index,  ArrayList<Integer> list){
+        
+        System.out.print(list.toString()+" ");
+
+        for(int i=index; i<arr.length; i++){
+            if(i>index && arr[i] == arr[i-1]) continue;
+            list.add(arr[i]);
+            
+            subset(arr, i+1, list);
+
+            list.remove(list.size()-1);
+        }
+
+    }
+
     public static void main(String[] args) {
-        int arr[] = {3,1,2};
+        int arr[] = {1,2,2,3};
         ArrayList<Integer> list = new ArrayList<>();
 
-        subSum(arr, 0, 0, list);
+        subset(arr, 0, list);
     }
 }
