79010f80d22f4de5b887055d0370fea7207da902
yashV0201 <vardhan.yash0201@gmail.com>
2024-08-05 13:00:01 +0530

    top and bottom views in a binary tree

--- BinarySearchTrees/BinTrees/bottomView.java
+++ BinarySearchTrees/BinTrees/bottomView.java
@@ -4,17 +4,7 @@ import java.util.List;
 import java.util.Queue;
 import java.util.TreeMap;
 
-class Tuple{
-    Node node;
-    int x;
-
-    Tuple(Node node, int x){
-        this.node = node;
-        this.x = x;
-    }
-}
-
-public class topView {
+public class bottomView {
     public static Node buildTree(int[]a){
         Node root = null;
 
@@ -41,35 +31,9 @@ public class topView {
         return root;
     }
 
-    public static List<Integer> top(Node root){
-        
-        TreeMap<Integer, Integer> verticals = new TreeMap<>();
-        Queue<Tuple> q = new LinkedList<Tuple>();
-        List<Integer> list = new ArrayList<>();
-        q.add(new Tuple(root, 0));
-        while(!q.isEmpty()){
-            Tuple t= q.poll();
-            Node node = t.node;
-            int x = t.x;
+    
 
-
-            if(!verticals.containsKey(x)){
-                verticals.put(x, node.val);
-            }
-
-            if(node.left != null) q.add(new Tuple(node.left, x-1));
-            if(node.right != null) q.add(new Tuple(node.right, x+1));
-
-        }
-
-        for(Integer a : verticals.values()){
-            list.add(a);
-        }
-
-        return list;
-    }
-
-    public static ArrayList <Integer> bottomView(Node root)
+    public static ArrayList <Integer> bottom(Node root)
     {
         // Code here
         TreeMap<Integer, Integer> verticals = new TreeMap<>();
@@ -101,7 +65,8 @@ public class topView {
     
     public static void main(String[] args) {
         Node root = buildTree(new int[]{8,5,9,3,2,4,1,6,7});
-        System.out.println(top(root).toString());
-        System.out.println(bottomView(root).toString());
+       
+        System.out.println(bottom(root).toString());
     }
+    
 }
